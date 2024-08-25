6fbc3e9f7ca5da37b842014890eded0c93eb8462
OmowunmiKamil <balikiskamil@gmail.com>
2024-08-25 07:12:20 +0100

    first commit

---
+++ src/scrip.js
@@ -0,0 +1,12 @@
+/*
+// Check if the page has been visited before
+if (localStorage.getItem("visitedSummary")) {
+  // Add the background color class if the page has been visited
+  document.getElementById("summaryButton").classList.add("bg-p-lightBlue");
+}
+
+// Store the page visit state when the page is loaded
+window.addEventListener("load", function () {
+  // Save the visited state in localStorage
+  localStorage.setItem("visitedSummary", "true");
+});*/
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        p: {
+          marineBlue: "hsl(213, 96%, 18%)",
+          purplishBlue: "hsl(228, 100%, 84%)",
+          pastelBlue: "hsl(228, 100%, 84%)",
+          lightBlue: "hsl(206, 94%, 87%)",
+          strawberryRed: "hsl(354, 84%, 57%)",
+        },
+        n: {
+          coolGray: "hsl(231, 11%, 63%)",
+          lightGray: "hsl(229, 24%, 87%)",
+          magnolia: "hsl(217, 100%, 97%)",
+          alabaster: "hsl(231, 100%, 99%)",
+          white: "hsl(0, 0%, 100%)",
+        },
+      },
+      fontFamily: {
+        ubuntu: "var(--fontUbuntu)",
+      },
+      backgroundImage: {
+        "bg-sidebar-desktop": "url('/assets/images/bg-sidebar-desktop.svg')",
+        bg: "url('/assets/images/bg-sidebar-mobile.svg')",
+      },
+      height: {
+        123: "31rem",
+      },
+    },
+  },
+  plugins: [],
+};
